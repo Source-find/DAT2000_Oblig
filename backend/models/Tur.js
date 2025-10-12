c5011b72260c32885b0ce6518ddb50165fc5a9c1
Jonathan Angell Pedersen <jonathan.pedersen09@gmail.com>
2025-10-12 11:21:58 +0200

    Bruker.js, Tur.js og mapper
    Opprettet mongoose schemas for Tur og Bruker. Laget noen mapper i /backend.

---
+++ backend/models/Tur.js
@@ -0,0 +1,53 @@
+// Vi lager et mongoose objekt
+const mongoose = require("mongoose")
+
+// Vi lager et TurSchema med mongoose
+const TurSchema = new mongoose.Schema({
+    navn: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    datoer: {
+        // Date-array
+        type: [Date],
+        required: true
+    },
+    destinasjon: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    type: {
+        type: String,
+        required: true,
+        enum: ["fjelltur", "skogstur"]
+    },
+    turLengde: {
+        type: String,
+        required: true,
+        enum: ["kort", "middels", "lang"]
+    },
+    vanskelighetsgrad: {
+        type: String,
+        required: true,
+        enum: ["lett", "middels", "vanskelig"]
+    },
+    lederId: {
+        // type må være en ObjectID tilhørende
+        // en bruker i "bruker"-modellen
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Bruker",
+        required: true
+    },
+    deltakerIds: {
+        // type må være en ObjectID tilhørende
+        // bruker/e i "bruker"-modellen. Hence arraybrackets.
+        // 
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: "Bruker"
+    }
+})
+
+// Eksporterer modellen slik at den kan brukes senere
+module.exports = mongoose.model('Tur', TurSchema);
